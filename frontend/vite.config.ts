98073bdf35a6d93b29e0f5526739f73bd5c80c20
mustafalazzawe <mustafa.alazzawe@gmail.com>
2024-12-25 12:44:24 -0500

    More vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://mustafalazzawe.github.io/",
+  base: "https://mustafalazzawe.github.io/devil-fruit-database/",
   plugins: [react()],
 });
