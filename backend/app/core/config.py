8818bc8369ad047672a01df8186eeeda24c7b1f3
mustafalazzawe <mustafa.alazzawe@gmail.com>
2025-01-07 23:46:27 -0500

    Updatiing allowed origins for cors

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -21,6 +21,7 @@ class Settings(BaseSettings):
         "http://localhost:5173",
         "http://0.0.0.0:5173",
         "http://127.0.0.1:5173",
+        "http://mustafalazzawe.github.io",
         "https://mustafalazzawe.github.io",
     ]
     ALLOWED_METHODS: list[str] = ["*"]
